2b7cfebb0775d84d65dc2d44915963a7ddd3d696
Eddie <eharriet@villanova.edu>
2021-01-30 13:13:51 -0800

    got jenkins working

--- scripts/bootstrap-jenkins.sh
+++ scripts/bootstrap-jenkins.sh
@@ -2,5 +2,8 @@
 
 
 k apply -f namespaces/jenkins-namespace.yaml
-k apply -f charts/jenkins/jenkins.volume.yaml
+k apply -f charts/jenkins/jenkins-volume.yaml
+k -njenkins create -f charts/jenkins/jenkins-volume-claim.yaml
+kubectl get secret --namespace jenkins jenkins -o jsonpath="{.data.jenkins-admin-password}" | base64 --decode
+
 helm install jenkins -f charts/jenkins/values.yaml jenkins/jenkins --namespace jenkins
